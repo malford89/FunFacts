f36cfcbfd0dc0d60806d70b0644650b4fee1ad5f
malford89 <michael.alford89@gmail.com>
2016-02-07 16:02:09 -0600

    Prevents the same value happening twice.

--- FunFacts/ViewController.h
+++ FunFacts/ViewController.h
@@ -13,6 +13,8 @@
 
 @interface ViewController : UIViewController
 
+- (UIColor*)getRandomColor:(UIColor*)currentBackgroundColor;
+- (NSString*)getRandomFact:(NSString*)currentFact;
 
 @property (weak, nonatomic) IBOutlet UILabel *funFactLabel;
 @property (strong, nonatomic) FactBook *factbook;
--- FunFacts/ViewController.m
+++ FunFacts/ViewController.m
@@ -33,10 +33,36 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)showFunFact {
-    UIColor *randomColor = [self.colorWheel randomColor];
+    // UIColor *randomColor = [self.colorWheel randomColor];
+    UIColor *randomColor = [self getRandomColor:self.view.backgroundColor];
     self.view.backgroundColor = randomColor;
     self.funFactButton.tintColor = randomColor;
-    self.funFactLabel.text = [self.factbook randomFact];
+    self.funFactLabel.text = [self getRandomFact:self.funFactLabel.text];
+}
+
+    //Prevents background color and fact from occuring twice in a row.
+    //The second fact/color is compared to the first generated fact/color.
+
+- (UIColor*)getRandomColor:(UIColor*)currentBackgroundColor {
+    UIColor *randomColor = [self.colorWheel randomColor];
+    if ([randomColor isEqual:currentBackgroundColor]) {
+        //The random fact is the same as the current fact so call this function again.
+        return [self getRandomColor:currentBackgroundColor]; //Recursion
+    } else {
+        return randomColor;
+        //If color is not equal to the last color then display the new generated color.
+    }
+}
+
+- (NSString*)getRandomFact:(NSString *)currentFact {
+    NSString *randomFact = [self.factbook randomFact];
+    if ([randomFact isEqual:currentFact]) {
+        //The random color is the same as the current color so call this function again.
+        return [self getRandomFact:currentFact];
+    } else {
+        return randomFact;
+        //If fact is not equal to the last fact then display the new generated fact.
+    }
 }
 
 @end
